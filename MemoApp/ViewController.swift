d63643f7768f58060aa7dfbadc1b51b8451a5397
Sh031224 <1cktmdgh2@gmail.com>
2020-12-07 13:54:21 +0900

    init

---
+++ MemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by 차승호 on 2020/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
